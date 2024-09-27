60e90ec44aa3827a4a2bfbf4d4a9c5589324e713
TomasSotomayor <to.sotomayor@duocuc.cl>
2024-09-27 17:24:40 -0300

    Instalando Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { AngularFireModule} from '@angular/fire/compat';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
