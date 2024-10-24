61ebce278089df8c9a32164c39735775c840c859
MrDrenix <to.sotomayor@duocuc.cl>
2024-10-24 16:40:30 -0300

    xd

--- src/app/pages/viajes/viajes.page.ts
+++ src/app/pages/viajes/viajes.page.ts
@@ -8,29 +8,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./viajes.page.scss'],
 })
 export class ViajesPage {
-  costo?: string;
-  fecha?: string;
-  ubicacionorigen?: string;
-  ubicaciondestino?: string;
-  nropatente?: string;
+  costo: string = "";
+  fecha: string = "";
+  ubicacionorigen: string = "";
+  ubicaciondestino: string = "";
   navCtrl: any;
 
 
   constructor(
+    private firebase:FirebaseService,
+    private viajeService:ViajeService,
     private alertController: AlertController,
-    private router: Router
+    private router: Router,
+    private helper: HelperService
   ) { }
 
   ngOnInit() {
   }
 
-  volverButton() {
-    this.router.navigateByUrl('/listaviajes');
+  async addViaje() {
+    const userFirebase = await this.firebase.addViaje(this.correo, this.contrasena);
+    const token = await userFirebase.user?.getIdToken();
+    if(token) {
+      const req = await this.viajeService.addViaje({
+        p_costo: this.costo,
+        p_fecha: this.fecha,
+        p_ubicacionorigen: this.ubicacionorigen,
+        p_ubicaciondestino: this.ubicaciondestino,
+        token:token
+      })
+    }
+    await this.helper.showAlert("Viaje agregado correctamente.", "Información");
+    await this.router.navigateByUrl('listaviajes');
   }
 
   async mostrarAlerta() {
     // Comprobación básica de que los campos requeridos estén llenos
-    if (!this.costo || !this.fecha || !this.ubicacionorigen || !this.ubicaciondestino || !this.nropatente) {
+    if (!this.costo || !this.fecha || !this.ubicacionorigen || !this.ubicaciondestino) {
       const alert = await this.alertController.create({
         header: 'Error',
         message: 'Por favor, complete todos los campos.',
@@ -46,5 +60,9 @@ export class ViajesPage {
     });
 
     await alert.present();
+  }
+
+  volverButton() {
+    this.router.navigateByUrl('/listaviajes');
   }
 }
