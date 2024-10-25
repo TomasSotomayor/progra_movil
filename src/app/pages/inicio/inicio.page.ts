11febca79f4260c2d6352f3b8560522c5710aa1d
Jorge Oyanedel <jor.oyanedel@duocuc.cl>
2024-10-25 18:11:03 -0300

    animation incorporado!!! siii!!!!!!!!

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NavController, LoadingController } from '@ionic/angular';
+import { NavController, LoadingController, AnimationController, Animation } from '@ionic/angular';
 
-import { ViewDidEnter, ViewDidLeave, ViewWillEnter, ViewWillLeave } from '@ionic/angular';
-import { ElementRef, ViewChildren, ViewChild } from '@angular/core';
-import type { QueryList } from '@angular/core';
-import type { Animation } from '@ionic/angular';
-import { AnimationController, IonCard } from '@ionic/angular';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { StorageService } from 'src/app/services/storage.service';
@@ -14,90 +9,33 @@ import { UserModel } from 'src/app/models/usuario';
 import { ViajeService } from 'src/app/services/viaje.service';
 import { HelperService } from 'src/app/services/helper.service';
 
-
-
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 
-export class InicioPage implements OnInit, ViewWillEnter, ViewDidEnter, ViewWillLeave, ViewDidLeave {
-
+export class InicioPage implements OnInit {
   correo: string = "";
   loaded: boolean = false;
   usuario: UserModel[] = [];
   viajes: any[] = [];
 
-
-
-  @ViewChild(IonCard, { read: ElementRef }) card: ElementRef<HTMLIonCardElement> | undefined;
+  @ViewChild('card', { read: ElementRef }) card: ElementRef<HTMLIonCardElement> | undefined;
   private animation: Animation | undefined;
 
-  constructor(private activateRoute:ActivatedRoute,
-    private router:Router,
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private router: Router,
     private animationCtrl: AnimationController,
-    private firebase:FirebaseService,
-    private usuarioService:UsuarioService,
-    private storage:StorageService,
-    private viajeService:ViajeService,
-    private helper:HelperService,
+    private firebase: FirebaseService,
+    private usuarioService: UsuarioService,
+    private storage: StorageService,
+    private viajeService: ViajeService,
+    private helper: HelperService,
     private navCtrl: NavController,
     private loadingController: LoadingController
-  ) { }
-
-
-
-  seleViaje(paramId:number) {
-    console.log("Viaje seleccionado: ", paramId);
-  }
-
-  async loadViajes() {
-    let dataStorage = await this.storage.obtenStorage();
-    const req = await this.viajeService.obtenViaje(dataStorage[0].token);
-    this.viajes = req.data;
-  }
-
-  async loadUsuario() {
-    let dataStorage = await this.storage.obtenStorage();
-    const req = await this.usuarioService.obtenUsuario(
-      {
-        p_correo:dataStorage[0].usuario_correo,
-        token:dataStorage[0].token
-      }
-    );
-    this.usuario = req.data;
-    console.log("Data inicio usuario: ", this.usuario);
-  }
-
-
-
-  ionViewDidLeave(): void {
-    console.log("view did leave");
-    
-  }
-  ionViewWillLeave(): void {
-    console.log("view will leave");
-    
-  }
-  ionViewDidEnter(): void {
-    console.log("view did enter");
-    if (this.card) {
-      this.animation = this.animationCtrl
-      .create()
-      .addElement(this.card.nativeElement)
-      .duration(1500)
-      .iterations(Infinity)
-      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
-      .fromTo('opacity', '1', '0.2');
-    }
-  }
-
-  ionViewWillEnter(): void {
-   console.log("view will enter");
-  }
-
-
+  ) {}
 
   ngOnInit() {
     this.loadUsuario();
@@ -106,12 +44,48 @@ export class InicioPage implements OnInit, ViewWillEnter, ViewDidEnter, ViewWill
     console.log("Parámetro URL: ", this.correo);
     setTimeout(() => {
       this.loaded = true;
-    },4000)
+    }, 4000);
+  }
+
+  async loadUsuario() {
+    let dataStorage = await this.storage.obtenStorage();
+    const req = await this.usuarioService.obtenUsuario({
+      p_correo: dataStorage[0].usuario_correo,
+      token: dataStorage[0].token
+    });
+    this.usuario = req.data;
+    console.log("Data inicio usuario: ", this.usuario);
+  }
+
+  async loadViajes() {
+    let dataStorage = await this.storage.obtenStorage();
+    const req = await this.viajeService.obtenViaje(dataStorage[0].token);
+    this.viajes = req.data;
+  }
+
+  ionViewDidEnter(): void {
+    console.log("view did enter");
+    if (this.card) {
+      // Crear la animación de cambio de color
+      this.animation = this.animationCtrl
+        .create()
+        .addElement(this.card.nativeElement)
+        .duration(2000) // Duración de 2 segundos para el cambio de color
+        .iterations(Infinity) // Repetir indefinidamente
+        .direction('alternate') // Alterna entre los colores
+        .keyframes([
+          { offset: 0, backgroundColor: 'blue' },
+          { offset: 0.5, backgroundColor: 'red' },
+          { offset: 1, backgroundColor: 'green' }
+        ]);
+
+      this.animation.play();
+    }
   }
 
   async cerrarSesion() {
-    const confirmo = await this.helper.showConfirm("¿Está seguro de que quiere cerrar sesión?")
-    if(confirmo) {
+    const confirmo = await this.helper.showConfirm("¿Está seguro de que quiere cerrar sesión?");
+    if (confirmo) {
       this.firebase.cerrarSesion();
       this.router.navigateByUrl('/login');
       const loading = await this.loadingController.create({
