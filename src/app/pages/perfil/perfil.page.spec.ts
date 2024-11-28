705c539d8bfdcb596987c2687abe8a3809f3fd3d
MrDrenix <to.sotomayor@duocuc.cl>
2024-11-28 16:31:12 -0300

    Test completados

--- src/app/pages/perfil/perfil.page.spec.ts
+++ src/app/pages/perfil/perfil.page.spec.ts
@@ -1,11 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PerfilPage } from './perfil.page';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 describe('PerfilPage', () => {
   let component: PerfilPage;
   let fixture: ComponentFixture<PerfilPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
+      providers: [provideHttpClient()]
+    });
     fixture = TestBed.createComponent(PerfilPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/viajes/viajes.page.spec.ts
+++ src/app/pages/viajes/viajes.page.spec.ts
@@ -1,11 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViajesPage } from './viajes.page';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 describe('ViajesPage', () => {
   let component: ViajesPage;
   let fixture: ComponentFixture<ViajesPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
+      providers: [provideHttpClient()]
+    });
     fixture = TestBed.createComponent(ViajesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
