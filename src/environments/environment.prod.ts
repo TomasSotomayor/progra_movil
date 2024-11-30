21ac477d4727c4e4e2b955b17e463cda5dd9ed38
Jorge Oyanedel <jor.oyanedel@duocuc.cl>
2024-11-29 21:55:32 -0300

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCFuQHBgbZjXp5xbFEuOUkAy-oW5xWYiAY",
+    authDomain: "oyanedel-sotomayor.firebaseapp.com",
+    projectId: "oyanedel-sotomayor",
+    storageBucket: "oyanedel-sotomayor.appspot.com",
+    messagingSenderId: "215322682992",
+    appId: "1:215322682992:web:920c40c2bf44906efa36b5",
+  },
+  apiUrl: "https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/"
 };
